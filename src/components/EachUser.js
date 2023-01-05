53bafd15ed939e7f9c32df45cf54ca6e7fd5c6f4
Rishu Singh <rishu.singh@futuretic.co.in>
2023-01-05 19:57:41 +0530

    second commit

--- src/components/EachUser.js
+++ src/components/EachUser.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import './UserList.css'
 import { HiOutlineMail } from 'react-icons/hi'
 import { CgProfile } from 'react-icons/cg'
+import Loader from './Loader';
 
 const EachUser = ({ userid }) => {
 
@@ -15,21 +16,24 @@ const EachUser = ({ userid }) => {
     return (
         <div>
             {
-                !singleUserState.loading
-                &&
-                <div className='usercard'>
-                    <div className="cardleft">
-                        <img src={singleUserState.singleuser.data.avatar} alt='DP' width='70%' />
-                    </div>
-                    <div className="cardright">
-                        <div className='namesection'>
-                            <CgProfile /><p>{singleUserState.singleuser.data.first_name} {singleUserState.singleuser.data.last_name}</p>
+                singleUserState.loading
+                    ?
+                    <Loader />
+                    :
+                    <div className='usercard'>
+                        <div className="cardleft">
+                            <img src={singleUserState.singleuser.data.avatar} alt='DP' width='70%' />
                         </div>
-                        <div className='emailsection'>
-                            <HiOutlineMail /> <p>{singleUserState.singleuser.data.email} </p>
+                        <div className="cardright">
+                            <div className='namesection'>
+                                <CgProfile /><p>{singleUserState.singleuser.data.first_name} {singleUserState.singleuser.data.last_name}</p>
+                            </div>
+                            <div className='emailsection'>
+                                <HiOutlineMail /> <p>{singleUserState.singleuser.data.email} </p>
+                            </div>
                         </div>
                     </div>
-                </div>
+
             }
         </div>
     )
